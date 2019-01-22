0b922d90b10a27fa7d7ef7c68a30a364bfe5d84d
Sudikshya-Bisoyi-Sheridan <bisoyi@sheridancollege.ca>
2019-01-22 11:13:48 -0500

    Initial Commit

---
+++ src/readandreverse/ReadAndReverse.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package readandreverse;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author shaim
+ */
+public class ReadAndReverse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please enter a name: ");
+        
+        String name = sc.nextLine();
+        
+       // System.out.println("You entered " + name);
+        
+        char[] nameInChar = new char[name.length()];
+        
+        // populate the arraty 
+        for(int i=0; i <= name.length()-1 ; i++ )
+            nameInChar[i] = name.charAt(i);
+        /*
+        // verify the array was populated properly
+        for(int i=0; i< name.length() ; i++)
+            System.out.println(nameInChar[i]);
+        */
+        
+        for(int i=nameInChar.length - 1 ; i >= 0 ; i--)
+            System.out.println(nameInChar[i]);
+        
+    }
+    
+}
